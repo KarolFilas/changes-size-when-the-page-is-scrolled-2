1e87c0ff6ba96d237313029f95580927fe510cde
Karol Filas <filaskarol@gmail.com>
2023-03-18 13:21:40 +0100

    colected data and creted div

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
 const squere = document.createElement('div')
 document.body.appendChild(squere)
-let size = 100;
-let grow = true
-squere.style.width = size + 'px'
-squere.style.height = size + 'px'
-squere.style.backgroundColor = 'yellowgreen'
 
-const sizeChange = () => {
+squere.style.height = '333px';
+squere.style.weight = '10px';
+squere.style.backgroundColor = 'green';
 
-    if (size >= window.innerWidth / 4) {
-        grow = false
-    } else if (size <= 0) {
-        grow = true
-    }
-
-    if (grow == true) {
-        size += 8
-        squere.style.width = size + 'px'
-        squere.style.height = size + 'px'
-    } else {
-        size -= 8
-        squere.style.width = size + 'px'
-        squere.style.height = size + 'px'
-    }
-}
-
-window.addEventListener('scroll', sizeChange)
+document.body.style.margin = '0px'
