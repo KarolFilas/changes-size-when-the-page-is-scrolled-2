534d3a46f7d52c9b58c5812d5df994dcef402f3a
Karol Filas <filaskarol@gmail.com>
2023-03-19 10:51:00 +0100

    finished project

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const squere = document.createElement('div')
 document.body.appendChild(squere)
-
-squere.style.height = '333px';
-squere.style.weight = '10px';
 squere.style.backgroundColor = 'green';
-
 document.body.style.margin = '0px'
+let grow = true
+let size = 10
+squere.style.height = size + 'px';
+
+const scrollFun = () => {
+
+    if (size >= window.innerWidth / 4) {
+        grow = false
+    } else if (size <= 10) {
+        grow = true
+    }
+
+    if (grow === true) {
+        size += 5;
+        squere.style.height = size + 'px';
+    } else if (grow === false) {
+        size -= 5;
+        squere.style.height = size + 'px';
+    }
+
+}
+
+
+
+
+
+
+
+window.addEventListener('scroll', scrollFun)
+
+
